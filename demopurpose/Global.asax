7b93b7ac2e2a284ba68792b0c74c1c63f89b82af
Rasmus <Rasmus@DESKTOP-97A1KG0>
2016-11-09 23:24:48 +0100

    Add project files.

---
+++ demopurpose/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="demopurpose.MvcApplication" Language="C#" %>
---
+++ demopurpose/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(demopurpose.Startup))]
+namespace demopurpose
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
