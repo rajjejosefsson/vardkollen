d9b6a29c45cb3e7e8b433d427bb16c2660234abc
Rasmus <Rasmus@DESKTOP-97A1KG0>
2016-12-06 16:33:23 +0100

    Added RelativeConnection view

--- CareCheck.DomainClasses/Relative.cs
+++ CareCheck.DomainClasses/Relative.cs
@@ -4,6 +4,7 @@ namespace CareCheck.DomainClasses
 {
     public class Relative
     {
+
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
